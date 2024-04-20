693894497e042c25c0ea62ca06121c5f46591cf0
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-04-21 01:26:11 +0330

    comment and blog apps initialized

--- Config/settings.py
+++ Config/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'drf_yasg',
+
     'account.apps.AccountConfig',
-
-
+    'blog.apps.BlogConfig',
+    'comment.apps.CommentConfig',
 ]
 
 MIDDLEWARE = [
