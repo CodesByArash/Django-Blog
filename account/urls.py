5ffbe4ece61253d10fe17167f0dfaed9826fee4f
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-04-20 14:00:51 +0330

    register url and forget password url configured

--- account/urls.py
+++ account/urls.py
@@ -6,9 +6,10 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    # path('register/', Register.as_view()),
-    path('forget-password/', PasswordForgetView.as_view(), name='forget-password'),
+    path('register/', Register.as_view()),
+    path('forget-password/', PasswordForget.as_view(), name='forget-password'),
     path('change-password/', PasswordChange.as_view(), name='change-password'),
     path('token/revoke/', TokenRevoke.as_view(), name='revoke-token'),
     path('verify-email/', EmailVerification.as_view(), name='verify-email'),
+    
 ]
