9981483dfede226a83961a3a10f106bb019d74a0
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-04-23 00:55:39 +0330

    comment urls modified

--- comment/urls.py
+++ comment/urls.py
@@ -2,14 +2,9 @@ from .views import *
 from django.contrib import admin
 from django.urls import path,include
 
-
 app_name = "comment"
 
-
-
-
-
 urlpatterns = [
-
-
+    path('articles/<slug:slug>/comments/', ArticleCommentsListView.as_view(), name="article-comments"),
+    path('articles/<slug:slug>/comments/<str:id>/', CommentDetailView.as_view(), name="article-comment-detail"),
 ]
