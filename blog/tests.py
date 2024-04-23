a3c7a37e84fc2b9f1845e578de769e1493ac7822
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-04-24 02:29:04 +0330

    comment and blog all views tests created

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,176 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+from django.urls import reverse
+
+from blog.models import Category, Article
+from account.models import User
+from comment.models import Comment
+from .views import *
+
+
+class ArticleListViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.article  = Article(title="test", slug="test", author=self.user, content="test")
+        self.article.categories.add(self.category)
+        self.article.save()
+        self.url      = reverse('blog:articles')
+
+    def test_articles_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_articles_post(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.post(self.url,data={"title":"test1","category_names":["test"],"slug":"test1","content":"test updated"},format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class ArticleDetailViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.article  = Article(title="test", slug="test", author=self.user, content="test")
+        self.article.categories.add(self.category)
+        self.article.status=True
+        self.article.save()
+
+        self.url      = reverse('blog:article-detail',kwargs={'slug':'test'})
+
+    def test_article_detail_get(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_article_detail_put(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.put(self.url,data={"title":"test1","category_names":["test"],"slug":"test1","content":"test updated"},format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_article_detail_delete(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class CategoryListViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.is_staff = True
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.url      = reverse('blog:categories')
+
+    def test_categories_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_categories_post(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.post(self.url,data={"name":"test1","description":"test1"},format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class CategoryDetailViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.is_staff = True
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.article  = Article(title="test", slug="test", author=self.user, content="test")
+        self.article.categories.add(self.category)
+        self.article.status=True
+        self.article.save()
+
+        self.url      = reverse('blog:category-detail',kwargs={"name":'test'})
+
+    def test_category_detail_get(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_category_detail_put_ok(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.put(self.url,data={"name":"test1","description":"test1"},format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_category_detail_delete_ok(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_category_detail_put_403(self):
+        self.user.is_staff = False
+        self.user.save()
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.put(self.url,data={"name":"test1"},format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_category_detail_delete_403(self):
+        self.user.is_staff = False
+        self.user.save()
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class CategoryArticleListViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.article  = Article(title="test", slug="test", author=self.user, content="test")
+        self.article.categories.add(self.category)
+        self.article.status=True
+        self.article.save()
+        self.url      = reverse('blog:category-articles',kwargs={"name":'test'})
+
+    def test_category_articles_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class SearchListViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.article  = Article(title="test", slug="test", author=self.user, content="test")
+        self.article.categories.add(self.category)
+        self.article.status=True
+        self.article.save()
+        self.url      = reverse('blog:search')+"?search=test"
+
+    def test_category_articles_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
--- comment/tests.py
+++ comment/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from django.urls import reverse
+
+from blog.models import Category, Article
+from account.models import User
+from comment.models import Comment
+from .views import CommentDetailView,ArticleCommentsListView
 
 # Create your tests here.
+class ArticleCommentsListViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.article  = Article(title="test", slug="test", author=self.user, content="test")
+        self.article.categories.add(self.category)
+        self.article.save()
+        self.comment  = Comment(article=self.article, author=self.user, content="test")
+        self.comment.save()
+        self.url      = reverse('comment:article-comments',kwargs={'slug':'test'})
+        
+    def test_comment_list_ok(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_comment_post_ok(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.post(self.url,data={"content":"test"},format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class CommentsDetailViewTestCase(APITestCase):
+    def setUp(self):
+        self.user     = User(username="test", email="test@test.com", password="test")
+        self.user.save()
+        self.category = Category(name="test", description="test")
+        self.category.save()
+        self.article  = Article(title="test", slug="test", author=self.user, content="test")
+        self.article.categories.add(self.category)
+        self.article.save()
+        self.comment  = Comment(article=self.article, author=self.user, content="test")
+        self.comment.save()
+        self.url      = reverse('comment:article-comment-detail',kwargs={'slug':'test','id':self.comment.id})
+
+    def test_comment_detail_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_comment_detail_put(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.put(self.url,data={"content":"test updated"},format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_comment_detail_delete(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
