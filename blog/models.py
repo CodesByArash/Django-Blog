28ab2ddc3bd4f4140df855112cbdaa716a2ea092
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-04-23 23:59:15 +0330

    category foreign key that was commented was deleted

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,6 @@ class Article(models.Model):
     title       = models.CharField(max_length = 50, blank = False, null = False)       
     slug        = models.SlugField(max_length=50, blank = False, null = False, unique = True )
     author      = models.ForeignKey(User,on_delete=models.CASCADE)
-    # category  = models.ForeignKey(Category,on_delete=models.CASCADE)
     content     = models.TextField(null=False,blank=False)
     publish     = models.DateTimeField(default = timezone.now)
     created     = models.DateTimeField(auto_now_add=True)
