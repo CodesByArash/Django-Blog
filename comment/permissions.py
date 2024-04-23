bb8898ae7e952459069b0ae0afdcd6028aa81fb2
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-04-23 19:59:16 +0330

    comment permissionclass created

--- comment/permissions.py
+++ comment/permissions.py
@@ -1,7 +1,19 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+
 class IsAuthenticatedOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(request.method in SAFE_METHODS or 
         request.user.is_authenticated
+        )
+    
+class CommentPermissionClass(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(
+            (request.user.is_authenticated and 
+            request.user.is_staff and
+            request.method == "DELETE") or
+            (request.user.is_authenticated and obj.author == request.user)
         )
