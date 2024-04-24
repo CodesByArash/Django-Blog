ef5772e28f9fd139a30d40de96f3c987aff0c03a
Arashmgn <81518981+Arashmgn@users.noreply.github.com>
2024-04-24 20:34:31 +0330

    password reset urls and password reset request urls generated

--- account/urls.py
+++ account/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('token/revoke/', TokenRevokeView.as_view(), name='token_revoke'),
     path('users/change-password/', PasswordChangeView.as_view(), name='change-password'),
-    path('users/forget-password/', PasswordForgetView.as_view(), name='forget-password'),
+    path('users/request-reset-password/', RequestPasswordResetView.as_view(), name='request-reset-password'),
+    path('users/reset-password/', PasswordResetView.as_view(), name='reset-password'),
     path('users/verify-email/', EmailVerificationView.as_view(), name='verify-email'),
 ]
 
